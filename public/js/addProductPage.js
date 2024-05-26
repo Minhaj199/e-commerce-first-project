c8b380d02e1d4ee10557ca07c659a0ef316269d0
Minhaj199 <150225109+Minhaj199@users.noreply.github.com>
2024-05-26 14:48:53 +0530

    crop with somet bug

--- public/js/addProductPage.js
+++ public/js/addProductPage.js
@@ -1,253 +1,172 @@
-
-
 let modal = document.getElementById("myModal");
 let close = document.querySelector(".close");
-let cropper
+let cancelBtn = document.getElementById("cancel-modal"); 
+let cropper;
 let dataURL;
+const fieldCounts = [0, 0, 0, 0, 0]; const minAspectRatio = 1; 
 
-console.log(close)
-close.addEventListener('click',()=>{
-  modal.style.display='none'
-   document.querySelector(".main-content").style.display = "block";
-   document.getElementById("cropeCanvas").style.display = "none";
-  if(cropper){
-    cropper.destroy()
-    cropper=null
+const fields = ["field-1", "field-2", "field-3", "field-4", "field-5"];
+const clickMeIds = [
+  "click-me-1",
+  "click-me-2",
+  "click-me-3",
+  "click-me-4",
+  "click-me-5",
+];
+const imgIds = [
+  "field-1-img",
+  "field-2-img",
+  "field-3-img",
+  "field-4-img",
+  "field-5-img",
+];
+const closeContainerIds = [
+  "close-container",
+  "close-container-2",
+  "close-container-3",
+  "close-container-4",
+  "close-container-5",
+];
+
+close.addEventListener("click", () => {
+  closeModal();
+});
+
+cancelBtn.addEventListener("click", () => {
+  closeModal(); // Call the closeModal function when cancel button is clicked
+});
+
+fields.forEach((fieldId, index) => {
+  document
+    .getElementById(fieldId)
+    .addEventListener("change", (event) => handleFieldChange(event, index));
+  document
+    .getElementById(closeContainerIds[index])
+    .addEventListener("click", () => resetField(index));
+});
+
+function handleFieldChange(event, index) {
+  let cropBtn = document.getElementById("crop");
+
+  removeClase();
+  cropBtn.classList.add(`f-${index + 1}`);
+
+  if (cropper) {
+    cropper.destroy();
+    cropper = null;
   }
 
-})
-
-let count_1 = 0;
-let count_2 = 0;
-let count_3 = 0;
-let count_4 = 0;
-let count_5 = 0;
-const field_1 = document.getElementById("field-1");
-
-field_1.addEventListener("change", function (event) {
-
-  if (count_1 === 0) {
-    modal.style.display = "block";
+  if (fieldCounts[index] === 0) {
+    openModal();
     const file = event.target.files[0];
     if (file) {
       const image = document.getElementById("img-t-A");
-      count_1++;
+      fieldCounts[index]++;
       const reader = new FileReader();
       reader.onload = function (e) {
         image.src = e.target.result;
-         cropper = new Cropper(image, {
-           aspectRatio: 0,
-           viewMode: 0,
-           background: false, 
-           crop(event) {
-             const canvas = cropper.getCroppedCanvas({
-               fillColor: "transparent",
-             });
-              dataURL = canvas.toDataURL("image/jpeg"); 
-             
-           
-           },
-         });
+        cropper = new Cropper(image, {
+          aspectRatio: minAspectRatio, // Set initial aspect ratio
+          viewMode: 0,
+          background: false,
+          crop(event) {
+            let cropBoxData = cropper.getCropBoxData();
+            let aspectRatio = cropBoxData.width / cropBoxData.height;
+            if (aspectRatio < minAspectRatio) {
+              let newWidth = cropBoxData.height * minAspectRatio;
+              cropBoxData.left -= (newWidth - cropBoxData.width) / 2;
+              cropBoxData.width = newWidth;
+              cropper.setCropBoxData(cropBoxData);
+            }
+            const canvas = cropper.getCroppedCanvas({
+              fillColor: "transparent",
+            });
+            dataURL = canvas.toDataURL("image/jpeg");
+          },
+        });
       };
       reader.readAsDataURL(file);
-
-      
-
-      document.getElementById("crop").addEventListener('click',()=>{
-        if (cropper) {
-          let croppedImageDisplay = document.getElementById("field-1-img");
-          const canvas = cropper.getCroppedCanvas({
-            fillerColor: "transparent",
-          });
-          canvas.toBlob((blob) => {
-            const file = new File([blob], "cropped_image.png", {
-              type: "image/png",
-            }); 
-            const dataTransfer = new DataTransfer();
-            dataTransfer.items.add(file);
-            field_1.files = dataTransfer.files;
-          }, "image/png");
-           croppedImageDisplay.src = dataURL;
-           const img = document.createElement("img");
-           img.src = "/images/Icons/delete_14025328.png";
-
-           img.classList.add("close-icons");
-           const container = document.getElementById("close-container");
-           container.appendChild(img);
-        }
-        modal.style.display='none'
-        if(cropper){
-          cropper.destroy()
-          cropper=null
-        }
-      });
-      
-      
     }
   } else {
-    const click_me = document.getElementById("click-me");
-    click_me.innerHTML = "Close image";
-    click_me.classList.add("click-me-added");
-    console.log(click_me);
-  }
-});
-document.getElementById("close-container").addEventListener("click", () => {
-  document.getElementById("field-1").value = "";
-  document.getElementById("field-1-img").src =
-    "../../images/Icons/cloud-upload-a30f385a928e44e199a62210d578375a.jpg";
-  document.getElementById("close-container").innerHTML = "";
-  count_1 = 0;
-  const click_me = document.getElementById("click-me");
-  click_me.innerHTML = "Click here";
-  click_me.classList.remove("click-me-added");
-  if(cropper){
-    cropper.destroy()
-    cropper=null
-  }
-});
-
-document.getElementById("field-2").addEventListener("change", function (event) {
-  const file = event.target.files[0];
-  if (count_2 === 0) {
-    count_2++;
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function (e) {
-        document.getElementById("field-2-img").src = e.target.result;
-      };
-      reader.readAsDataURL(file);
-      const img = document.createElement("img");
-      img.src = "/images/Icons/delete_14025328.png";
-      img.classList.add("close-icons");
-      const container = document.getElementById("close-container-2");
-      container.appendChild(img);
+    const clickMe = document.getElementById(clickMeIds[index])
+    if(clickMe){
+      clickMe.innerHTML = "Close image";
+      clickMe.classList.add("click-me-added");
     }
-  } else {
-    const click_me = document.getElementById("click-me-2");
-    click_me.innerHTML = "Close image";
-    click_me.classList.add("click-me-added");
+    
   }
-});
-document.getElementById("close-container-2").addEventListener("click", () => {
-  document.getElementById("field-2").value = "";
-  document.getElementById("field-2-img").src =
+
+  cropBtn.removeEventListener("click", handleCrop);
+  cropBtn.addEventListener("click", handleCrop);
+}
+
+function handleCrop() {
+  let cropBtn = document.getElementById("crop");
+  let activeFieldIndex = fields.findIndex((field, i) =>
+    cropBtn.classList.contains(`f-${i + 1}`)
+  );
+  if (cropper && activeFieldIndex !== -1) {
+    let croppedImageDisplay = document.getElementById(imgIds[activeFieldIndex]);
+    const canvas = cropper.getCroppedCanvas({ fillColor: "transparent" });
+    canvas.toBlob((blob) => {
+      const file = new File([blob], "cropped_image.png", { type: "image/png" });
+      const dataTransfer = new DataTransfer();
+      dataTransfer.items.add(file);
+      document.getElementById(fields[activeFieldIndex]).files =
+        dataTransfer.files;
+    }, "image/png");
+    croppedImageDisplay.src = dataURL;
+
+    const img = document.createElement("img");
+    img.src = "/images/Icons/delete_14025328.png";
+    img.classList.add("close-icons");
+    document
+      .getElementById(closeContainerIds[activeFieldIndex])
+      .appendChild(img);
+  }
+  closeModal();
+}
+
+function resetField(index) {
+  document.getElementById(fields[index]).value = "";
+  document.getElementById(imgIds[index]).src =
     "../../images/Icons/cloud-upload-a30f385a928e44e199a62210d578375a.jpg";
-  document.getElementById("close-container-2").innerHTML = "";
-  count_2 = 0;
-  const click_me = document.getElementById("click-me-2");
-  click_me.innerHTML = "Click here";
-  click_me.classList.remove("click-me-added");
-});
-
-document.getElementById("field-3").addEventListener("change", function (event) {
-  const file = event.target.files[0];
-  if (count_3 === 0) {
-    if (file) {
-      count_3++;
-      const reader = new FileReader();
-      reader.onload = function (e) {
-        document.getElementById("field-3-img").src = e.target.result;
-      };
-      reader.readAsDataURL(file);
-      const img = document.createElement("img");
-      img.src = "/images/Icons/delete_14025328.png";
-      img.classList.add("close-icons");
-      const container = document.getElementById("close-container-3");
-      container.appendChild(img);
-    }
-  } else {
-    const click_me = document.getElementById("click-me-3");
-    click_me.innerHTML = "Close image";
-    click_me.classList.add("click-me-added");
+  document.getElementById(closeContainerIds[index]).innerHTML = "";
+  fieldCounts[index] = 0;
+  const clickMe = document.getElementById(clickMeIds[index]);
+  if(clickMe){
+    clickMe.innerHTML = "Click here";
+    clickMe.classList.remove("click-me-added");
   }
-});
-document.getElementById("close-container-3").addEventListener("click", () => {
-  document.getElementById("field-3").value = "";
-  document.getElementById("field-3-img").src =
-    "../../images/Icons/cloud-upload-a30f385a928e44e199a62210d578375a.jpg";
-  document.getElementById("close-container-3").innerHTML = "";
-  count_3 = 0;
-  const click_me = document.getElementById("click-me-3");
-  click_me.innerHTML = "Click here";
-  click_me.classList.remove("click-me-added");
-});
-
-document.getElementById("field-4").addEventListener("change", function (event) {
-  const file = event.target.files[0];
-  if (count_4 === 0) {
-    if (file) {
-      count_4++;
-      const reader = new FileReader();
-      reader.onload = function (e) {
-        document.getElementById("field-4-img").src = e.target.result;
-      };
-      reader.readAsDataURL(file);
-      const img = document.createElement("img");
-      img.src = "/images/Icons/delete_14025328.png";
-      img.classList.add("close-icons");
-      const container = document.getElementById("close-container-4");
-      container.appendChild(img);
-    }
-  } else {
-    const click_me = document.getElementById("click-me-4");
-    click_me.innerHTML = "Close image";
-    click_me.classList.add("click-me-added");
+  
+  if (cropper) {
+    cropper.destroy();
+    cropper = null;
   }
-});
-document.getElementById("close-container-4").addEventListener("click", () => {
-  document.getElementById("field-4").value = "";
-  document.getElementById("field-4-img").src =
-    "../../images/Icons/cloud-upload-a30f385a928e44e199a62210d578375a.jpg";
-  document.getElementById("close-container-4").innerHTML = "";
-  count_4 = 0;
-  const click_me = document.getElementById("click-me-4");
-  click_me.innerHTML = "Click here";
-  click_me.classList.remove("click-me-added");
-});
+}
 
-document.getElementById("field-5").addEventListener("change", function (event) {
-  const file = event.target.files[0];
-  if (count_5 === 0) {
-    if (file) {
-      count_5++;
-      const reader = new FileReader();
-      reader.onload = function (e) {
-        document.getElementById("field-5-img").src = e.target.result;
-      };
-      reader.readAsDataURL(file);
-      const img = document.createElement("img");
-      img.src = "/images/Icons/delete_14025328.png";
-      img.classList.add("close-icons");
-      const container = document.getElementById("close-container-5");
-      container.appendChild(img);
-    }
-  } else {
-    const click_me = document.getElementById("click-me-5");
-    click_me.innerHTML = "Close image";
-    click_me.classList.add("click-me-added");
+function removeClase() {
+  let cropBtn = document.getElementById("crop");
+  for (let i = 0; i < fields.length; i++) {
+    cropBtn.classList.remove(`f-${i + 1}`);
   }
-});
-document.getElementById("close-container-5").addEventListener("click", () => {
-  document.getElementById("field-5").value = "";
-  document.getElementById("field-5-img").src =
-    "../../images/Icons/cloud-upload-a30f385a928e44e199a62210d578375a.jpg";
-  document.getElementById("close-container-5").innerHTML = "";
-  count_5 = 0;
-  const click_me = document.getElementById("click-me-5");
-  click_me.innerHTML = "Click here";
-  click_me.classList.remove("click-me-added");
-});
+}
 
-document.querySelector(".dis-input").addEventListener("input", function () {
-  const Text = this.value;
+function openModal() {
+  modal.style.display = "block";
+  document.querySelector(".main-content").style.display = "none";
+  document.getElementById("cropeCanvas").style.display = "block";
+}
 
-  if (Text.length > 70) {
-    let sid = document.querySelector(".dis-input").value.slice(0, 70);
-    console.log(sid);
-    document.querySelector(".dis-input").value = sid;
-    showToast("Word limit reached");
+function closeModal() {
+  modal.style.display = "none";
+  document.querySelector(".main-content").style.display = "block";
+  document.getElementById("cropeCanvas").style.display = "none";
+  if (cropper) {
+    cropper.destroy();
+    cropper = null;
   }
-});
+}
 
 function validate() {
   const name = document.getElementById("name").value;
@@ -282,28 +201,6 @@ function validate() {
   }
   return true;
 }
-
-///modal cancel button//
-
-document.getElementById("cancel-modal").addEventListener('click',()=>{
-   count_1 = 0;
-   count_2 = 0;
-   count_3 = 0;
-   count_4 = 0;
-   count_5 = 0;
-if(cropper){
-    cropper.destroy()
-    cropper=null
-  }
-  modal.style.display='none'
-});
-
-
-// Get the modal
-
-
-// Function to open the modal
-
 
 function showToast(message) {
   console.log("inside tost");
