701cce4b526be642d5ad2cff777a8e04bc1cb093
minhaj199 <minhajpuzhakkal@gmail.com>
2026-07-30 10:17:03 +0530

    feat: enhance request logging with additional headers and user information

--- middleware/requestLogger.js
+++ middleware/requestLogger.js
@@ -10,12 +10,18 @@ const requestLogger = (req, res, next) => {
 
   res.on("finish", () => {
     logger.info({
+      ip: req.ip,
       method: req.method,
       url: req.originalUrl,
+      userAgent: req.get("User-Agent"),
+      referer: req.headers["referer"],
+      origin: req.headers["origin"],
+      accept: req.headers["accept"],
+      acceptLanguage: req.headers["accept-language"],
+      secFetchSite: req.headers["sec-fetch-site"],
+      turnstilePresent: Boolean(req.body["cf-turnstile-response"]),
       status: res.statusCode,
       responseTime: Date.now() - start,
-      ip: req.ip,
-      userAgent: req.get("User-Agent"),
       contentLength: res.getHeader("Content-Length") || 0,
       timestamp: new Date().toISOString(),
     });
